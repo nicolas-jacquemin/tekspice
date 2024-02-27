b635465cd7679aea56e8176b726a17cb9f9f51ba
Zowks <cyffers.merlin@gmail.com>
2024-02-27 18:55:40 +0100

    feat: basic parser error handling

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@
 ##
 
 SRC		=	src/main.cpp \
+			src/parser.cpp \
 			src/Circuit.cpp \
 			src/Component.cpp \
 			src/Pin.cpp \
--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,12 @@
 
 #include "nts.hpp"
 
-void openConfigFile(const std::string &path) {
-    std::ifstream file(path);
-
-    if (!file.is_open())
-        throw nts::Error("Could not open file \"" + path + "\"");
-
-    std::string line;
-    std::string buffer;
-    while (std::getline(file, line))
-        buffer += line + '\n';
-
-    file.close();
-}
-
 int main(int argc, char **argv) {
     try {
         if (argc < 2)
             throw nts::Error("No configuration file provided as argument");
 
-        openConfigFile(argv[1]);
+        parser(argv[1]);
     } catch (const std::exception &e) {
         std::cerr << "\e[0;31m" << e.what() << '\n';
     }
